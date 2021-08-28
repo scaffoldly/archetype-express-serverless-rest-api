e0bbdb3640319aff9040035612a54ad2d7912c61
Christian Nuss <christian.nuss@gmail.com>
2021-08-27 21:21:02 -0700

    Version Updates
    @scaffoldly/serverless-uti
    @types/dynamodb

--- express/serverless/src/auth.ts
+++ express/serverless/src/auth.ts
@@ -1,6 +1,6 @@
 import { authorize } from '@scaffoldly/serverless-util';
 import { env } from './env';
 
-const DOMAIN = env.SERVERLESS_API_DOMAIN;
+const DOMAIN = env['api-gateway-domain'];
 
 export const expressAuthentication = authorize(DOMAIN);
