902caf5ebc3cbe53efb1a5e8a1a31181eb13cf14
Christian Nuss <christian.nuss@gmail.com>
2021-08-29 16:32:34 -0700

    Use repository-name

--- serverless/serverless.config.js
+++ serverless/serverless.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const envVars = require('./.scaffoldly/env-vars.json');
 
-module.exports.repositoryName = envVars['application-name'];
+module.exports.repositoryName = envVars['repository-name'];
 module.exports.serviceName = envVars['service-name'];
