9101829c8f6c96458631c946610f9f4316a27dc1
Christian Nuss <christian.nuss@gmail.com>
2021-08-29 15:41:48 -0700

    Add serverless.config.js

---
+++ serverless/serverless.config.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const envVars = require('./.scaffoldly/env-vars.json');
+
+module.exports.repositoryName = envVars['application-name'];
+module.exports.serviceName = envVars['service-name'];
