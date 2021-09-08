fe4d75da492f3ec392d9578e6949e236ddc7099d
Christian Nuss <christian.nuss@gmail.com>
2021-09-08 09:17:03 -0700

    Stage Domain

--- serverless/src/auth.ts
+++ serverless/src/auth.ts
@@ -1,6 +1,6 @@
 import { authorize } from '@scaffoldly/serverless-util';
 import { env } from './env';
 
-const DOMAIN = env['api-gateway-domain'];
+const DOMAIN = env['stage-domain'];
 
 export const expressAuthentication = authorize(DOMAIN);
